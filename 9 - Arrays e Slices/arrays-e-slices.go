f3885838ce0bfd1bcd347dc92f46f9bfe58a8d33
heliobarolo0586 <heliobarolo0586@gmail.com>
2022-11-08 17:39:17 -0300

    Finalizando slices e iniciando maps

--- 9 - Arrays e Slices/arrays-e-slices.go
+++ 9 - Arrays e Slices/arrays-e-slices.go
@@ -1,10 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Arrays e Slices")
 
-	var array1 [5]int
+	var array1 [5]string
+	array1[0] = "Posição "
 	fmt.Println(array1)
+
+	array2 := [5]string{"Posição 1", "Posição 2", "Posição 3", "Posição 4", "Posição 5"}
+	fmt.Println(array2)
+
+	array3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(array3)
+
+	slice := []int{10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	fmt.Println(slice)
+
+	// fmt.Println(reflect.TypeOf(slice))
+	// fmt.Println(reflect.TypeOf(array3))
+
+	slice = append(slice, 18)
+	fmt.Println(slice)
+
+	//Indice 1 é inclusivo e indice 2 excluido
+	slice2 := array2[1:3]
+	fmt.Println(slice2)
+
+	array2[1] = "Posição Alterada"
+	fmt.Println(slice2)
+
+	//Arrays Interno
+	fmt.Println("---------------------------")
+	slice3 := make([]float32, 10, 15)
+	fmt.Println(slice3)
+
+	slice3 = append(slice3)
+
+	fmt.Println(len(slice3)) // length
+	fmt.Println(cap(slice3)) // Capacidade
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
 }
